8fa1a2ffc4dab3af55c86ba4e8cff0bc854c8a77
Eugene <zgainer@gmail.com>
2023-01-10 09:11:59 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Eugene on 10.01.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
